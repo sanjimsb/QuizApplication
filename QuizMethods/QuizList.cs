d0e9a40e9605734f0fb76367f18b8824bfe68c5e
Vinith1697 <97vinith97@gmail.com>
2022-08-04 16:04:23 -0400

    pushed QuizMethods containing classes QuizList and ModuleList

---
+++ QuizMethods/QuizList.cs
@@ -0,0 +1,25 @@
+﻿using QuizDbContext;
+using QuizModels;
+
+namespace QuizMethods;
+
+public static class QuizList
+{
+    public static readonly QuizContext _db;
+
+    public static IList<Quiz> Quizes;
+
+
+    public static IList<Quiz> GetAllQuiz()
+    {
+        Quizes = _db.Quiz!.Select(q => q).ToList();
+        return Quizes;
+    }
+
+    public static IList<Quiz> GetQuizeByModule(int ModuleId)
+    {
+        Quizes = _db.Quiz!.Where(q => q.ModuleId == ModuleId).ToList();
+        return Quizes;
+    }
+}
+
