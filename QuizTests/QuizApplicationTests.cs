18eb24b93f9f2dd18fc3555f4933806bcf80b94c
Yash <yashkelkar09@gmail.com>
2022-08-04 22:34:51 -0400

    pushed QuizTests class library consisting class QuizApplicationTests

---
+++ QuizTests/QuizApplicationTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using QuizMethods;
+using QuizModels;
+
+namespace QuizTests;
+
+[TestClass]
+public class QuizApplicationTests
+{
+    
+    [TestMethod]
+    public void QuizListMethodExists()
+    {
+        MethodInfo info = typeof(QuizList).GetMethod("GetAllQuiz");
+        Assert.IsNotNull(info);
+    }
+
+    [TestMethod]
+    public void ModuleListMethodExists()
+    {
+        MethodInfo info = typeof(ModuleList).GetMethod("GetAllModules");
+        Assert.IsNotNull(info);
+    }
+
+    [TestMethod]
+    public void GetQuizeByModuleMethodExists()
+    {
+        MethodInfo info = typeof(QuizList).GetMethod("GetQuizeByModule");
+        Assert.IsNotNull(info);
+    }
+
+    [TestMethod]
+    public void QuizListMethodReturnsListOfQuiz()
+    {
+        IList<Quiz> quizes = QuizList.GetAllQuiz();
+        Assert.IsNotNull(quizes);
+    }
+}
