80f79966e2ccb161496b1c61ffa50f5a2b3fd77f
Bipin MSB <bpmsb45@gmail.com>
2022-08-11 23:08:32 -0400

    Added Account pages

---
+++ QuizApplication/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuizModels;
+
+namespace QuizApplication.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public User User;
+        public void OnGet()
+        {
+        }
+    }
+}
--- QuizModels/User.cs
+++ QuizModels/User.cs
@@ -18,5 +18,7 @@ public class User
     public string? Email { get; set; }
 
     [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
     public string? Password { get; set; }
+
 }
